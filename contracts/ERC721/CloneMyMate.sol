8cb0caafe285a032a572d532dd94e59b08f63d38
smlee <seongmooleekr@gmail.com>
2023-09-02 08:57:48 +0900

    CloneMyMate

--- contracts/ERC721/CloneMyMate.sol
+++ contracts/ERC721/CloneMyMate.sol
@@ -5,8 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "./Counters.sol";
 
 contract NeoHackathonNFT is ERC721, Pausable, Ownable, ERC721Burnable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdTracker;
     constructor() ERC721("NeoHackathonNFT", "NHN") {}
 
     function pause() public onlyOwner {
@@ -17,8 +20,9 @@ contract NeoHackathonNFT is ERC721, Pausable, Ownable, ERC721Burnable {
         _unpause();
     }
 
-    function safeMint(address to, uint256 tokenId) public {
-        _safeMint(to, tokenId);
+    function safeMint(address to) public {
+        _safeMint(to, _tokenIdTracker.current());
+        _tokenIdTracker.increment();
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
